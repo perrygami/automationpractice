3e62c9d18bd470a183019b8824546eefe587da21
Perry Gami <perry.g@simformsolutions.com>
2019-07-30 12:47:40 +0530

    Contatc PageTest

---
+++ src/test/java/com/crm/qa/testcases/ContactPageTest.java
@@ -0,0 +1,29 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.LoginPage;
+
+public class ContactPageTest extends TestBase {
+	
+	LoginPage loginpage;
+	
+
+public ContactPageTest() {
+		
+		super();
+		
+	}
+
+@BeforeMethod
+public void setup() {
+	
+	init();
+	loginpage = new LoginPage();
+	loginpage.Login(prop.getProperty("username"),prop.getProperty("password"));
+	
+}
+
+	@Test
+}
